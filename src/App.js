65e2862fcf8d3477a760a016749e63341587189b
João Paulo Antunes <48777730+JoaoPauloAntunes@users.noreply.github.com>
2022-07-14 19:26:32 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "leaflet/dist/leaflet.css";
+import "leaflet-area-select";
+import { MapContainer, TileLayer } from "react-leaflet";
+
+import AreaSelect from "./AreaSelect";
+import "./styles.css";
+
+export default function App() {
+  const position = [51.505, -0.09];
+
+  return (
+    <MapContainer center={position} zoom={13} style={{ height: "100vh" }}>
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <AreaSelect />
+    </MapContainer>
+  );
+}
